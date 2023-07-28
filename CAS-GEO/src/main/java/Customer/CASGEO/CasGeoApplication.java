f839d44f9b74837b140c35dcf1f49083006764ea
sumanth kollipara <sumanthkumar44@gmail.com>
2023-07-28 12:01:16 +0100

    Add files via upload
    initial commit

---
+++ CAS-GEO/src/main/java/Customer/CASGEO/CasGeoApplication.java
@@ -0,0 +1,13 @@
+package Customer.CASGEO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasGeoApplication.class, args);
+	}
+
+}
